f702b9199118093f1cf4748f8275b1888672cdc0
Nishant8209 <nishant.prabhu@techdenali.com>
2026-07-10 14:41:58 +0530

    added the one wishlist endpoint

--- src/router/wishListRoutes.ts
+++ src/router/wishListRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { addItemToWishlist, createWishlistGroup, getUserWishlistNames, getUserWishlists, removeBookFromWishlist } from "../controllers/wishListController";
+import { addItemToWishlist, createWishlistGroup, getUserWishlistNames, getUserWishlists, getUserWishlistsWithBooks, removeBookFromWishlist } from "../controllers/wishListController";
 
 const router = Router();
 
 router.get("/:userId", getUserWishlists);
+router.get("/getAllWishList/:userId", getUserWishlistsWithBooks);
 router.get("/wishlistName/:userId", getUserWishlistNames);
 router.post("/group", createWishlistGroup);
 router.post("/add", addItemToWishlist);
