8ba3f7a916e4d534e2e06adc0c7fb201c11283b4
Nishant8209 <nishant.prabhu@techdenali.com>
2026-07-13 16:32:25 +0530

    added wishlist thing

--- src/utils/buildWishlistBooksAggregationPipeline.ts
+++ src/utils/buildWishlistBooksAggregationPipeline.ts
@@ -1,40 +1,43 @@
 export const buildWishlistBooksAggregationPipeline = (
-  filter: any,
-  page: number,
-  limit: number,
+    filter: any,
+    page: number,
+    limit: number,
 ) => {
-  const skip = (page - 1) * limit;
+    const skip = (page - 1) * limit;
 
-  return [
-    { $match: filter },
-    {
-      $project: {
-        name: 1,
-        items: 1,
-      },
-    },
-    { $unwind: "$items" },
-    {
-      $lookup: {
-        from: "books",
-        localField: "items.bookId",
-        foreignField: "_id",
-        as: "book",
-      },
-    },
-    { $unwind: "$book" },
-    {
-      $project: {
-        wishlistName: "$name",
-        _id: "$book._id",
-        name: "$book.name",
-        description: "$book.description",
-        price: "$book.purchasePrice",
-        coverImage: "$book.coverImage",
-        author: "$book.author",
-      },
-    },
-    { $skip: skip },
-    { $limit: limit },
-  ];
+    return [
+        { $match: filter },
+        {
+            $project: {
+                name: 1,
+                items: 1,
+            },
+        },
+        { $unwind: "$items" },
+        {
+            $lookup: {
+                from: "books",
+                localField: "items.bookId",
+                foreignField: "_id",
+                as: "book",
+            },
+        },
+        { $unwind: "$book" },
+        {
+            $project: {
+                wishlistName: "$name",
+                _id: "$book._id",
+                name: "$book.name",
+                description: "$book.description",
+                price: "$book.purchasePrice",
+                coverImage: "$book.coverImage",
+                author: "$book.author",
+                rentalPricePerDay: "$book.rentalPricePerDay",
+                rentalPricePerWeek: "$book.rentalPricePerWeek",
+                rentalPricePerMonth: "$book.rentalPricePerMonth",
+            },
+        },
+        { $skip: skip },
+        { $limit: limit },
+    ];
 };
