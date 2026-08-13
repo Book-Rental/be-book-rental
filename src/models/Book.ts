4a58851a9efb144a48ceee533b30609dd90957d0
Nishant8209 <nishant.prabhu@techdenali.com>
2026-08-13 11:55:48 +0530

    added the return shipment

--- src/models/Book.ts
+++ src/models/Book.ts
@@ -32,6 +32,7 @@ export interface IBook extends IBasicFields {
     images?: Image[];
     isAvailable?: boolean;
     quantity: number;
+    auction: boolean;
 }
 
 const bookSchema = new Schema<IBook>({
@@ -131,6 +132,10 @@ const bookSchema = new Schema<IBook>({
             altText: { type: String, required: true },
         },
     ],
+    auction: {
+        type: Boolean,
+        default: false,
+    },
     isActive: {
         type: Boolean,
         default: true,
